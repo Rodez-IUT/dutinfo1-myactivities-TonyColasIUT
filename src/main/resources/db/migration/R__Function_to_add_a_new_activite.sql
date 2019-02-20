3ee4237533a2f455d8d724844c9ca301f888a559
Tony Colas <tony.colas@iut-rodez.fr>
2019-02-20 11:13:41 +0100

    Fix 2.2 function to add new activity OK

---
+++ src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
@@ -0,0 +1,6 @@
+DROP FUNCTION add_activity_with_title(varchar);
+CREATE OR REPLACE FUNCTION add_activity_with_title(title varchar(50)) RETURNS bigint AS $$
+    INSERT INTO activity (id,title)
+    VALUES (nextval('id_generator'), title)
+    RETURNING id;
+$$ LANGUAGE SQL
--- src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
+++ src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
@@ -1,5 +1,4 @@
-CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS int AS $$
+CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS void AS $$
    DELETE FROM activity
    WHERE modification_date < old_date;
-   SELECT 1;
 $$ LANGUAGE SQL; 
