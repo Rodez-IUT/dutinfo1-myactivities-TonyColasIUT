4a792d59890715ea812129b377a45f226a144709
Tony Colas <tony.colas@iut-rodez.fr>
2019-05-28 10:58:24 +0200

    fix 8.1.2 Project compiles

--- src/main/resources/db/migration/R__Trigger_to_log_action_on_activities.sql
+++ src/main/resources/db/migration/R__Trigger_to_log_action_on_activities.sql
@@ -1,13 +1,18 @@
+    
+DROP TRIGGER IF EXISTS log_delete_activity on activity ;
 
-CREATE OR REPLACE FUNCTION trigger_to_log_action() RETURNS trigger  AS $$
-DECLARE 
-
+-- fonction trigger
+CREATE OR REPLACE FUNCTION log_delete_activity()
+    RETURNS TRIGGER AS $$
 BEGIN
-    INSERT INTO action_log
-    SELECT nextval('id_generator'),'delete', 'activity', user, OLD.id;
-    RETURN NULL;
-END
-$$ LANGUAGE plpgsql;
+     INSERT INTO action_log (id, action_name, entity_name, entity_id, author, action_date)
+        values  (nextval('id_generator'),'delete', 'activity', OLD.id, user, now());
+     RETURN NULL; -- le résultat est ignoré car il s'agit d'un trigger AFTER
+END;
+$$ language plpgsql;
 
-CREATE TRIGGER trigger_to_log_action AFTER DELETE ON activity
-    FOR EACH ROW EXECUTE PROCEDURE trigger_to_log_action();
+
+-- trigger
+CREATE TRIGGER log_delete_activity
+    AFTER DELETE ON activity
+    FOR EACH ROW EXECUTE PROCEDURE log_delete_activity();
